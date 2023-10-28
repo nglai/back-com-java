8844f6d0c4a5279f90d4b62c2320b1ca40597a2c
Ng Lai <nglaikoin@hotmail.com>
2023-10-28 19:36:11 -0300

    adicionando o spring boot

---
+++ src/main/java/br/com/projetofull/backjava/BackjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetofull.backjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjavaApplication.class, args);
+	}
+
+}
